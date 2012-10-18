714cb13645c73e2b9de5b0d77b74c44d0d6d1216
Dan York <dyork@lodestar2.com>
2012-10-18 09:12:17 -0400

    Added a very simple example of using the REST API

---
+++ simple.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+#
+# A simple example of using the SIDN Labs DNSSEC Portfolio Checker at http://check.sidnlabs.nl:8080/form
+# via the REST API
+#
+
+import urllib2
+
+portfolio = ['internetsociety.org','ietf.org','sidnlabs.nl','google.com']
+
+for domain in portfolio:
+    status = urllib2.urlopen('http://check.sidnlabs.nl:8080/check/%s' % domain).read().split(',')[2]
+    print "%s - %s" % (domain, status)
+
+#
+# Regarding what is going on in the first line of the for loop, the result sent back from
+# the Portfolio Checker has this format:
+#
+# internetsociety.org,"",secure,"" 
+#
+# The "urllib2.urlopen().read()" retrieves that information.
+# ".split(',')" breaks the result into a list based on the comma
+# "[2]" references the third item in the list (because python lists start at 0)
+#
